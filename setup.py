7dceac5d98b9dbdc2c6ff626930ae8d62f071321
veiguf <veit.gufler@unibz.it>
2022-06-17 15:00:59 +0200

    setup.py changed

--- setup.py
+++ setup.py
@@ -15,20 +15,22 @@ def package_files(directory):
     return paths
 
 if __name__ == '__main__':
-    from distutils.core import setup
+    from setuptools import setup
     extra_files = package_files('EasyBeam')
-    setup(name='EasyBeam',
-          version='1.0.0',
-          description='Easy Application for Structural analYsis with BEAMs',
-          author='V. Gufler, E. J. Wehrle',
-          author_email='veit.gufler@unibz.it',
-          url='https://github.com/veiguf/EasyBeam',
-          package_data={'': extra_files},
-          license='GNU Lesser General Public License 3.0',
-          packages=['EasyBeam'],
-          copyright="Copyright 2020, 2021 V. Gufler",
-          install_requires=['numpy',
-                            'scipy',
-                            'matplotlib'],
-    long_description=long_description,
-    long_description_content_type='text/markdown')
+    setup(
+        name='EasyBeam',
+        version='1.1.1',
+        description='Easy Application for Structural analYsis with BEAMs',
+        author='V. Gufler, E. J. Wehrle',
+        author_email='veit.gufler@unibz.it',
+        url='https://github.com/veiguf/EasyBeam',
+        package_data={'': extra_files},
+        license='GNU Lesser General Public License 3.0',
+        packages=['EasyBeam'],
+        copyright='Copyright 2020-2022 V. Gufler',
+        install_requires=['numpy',
+                          'scipy',
+                          'matplotlib'],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+    )
